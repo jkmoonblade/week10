8b491b736be41cc711d8294bf415875bb062ec58
Joseph Keene <Joseph Keene>
2017-05-01 16:22:32 -0600

    Added Team Assignment 03

---
+++ web/ta03.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Welcome!<br>
+Your name is: <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your major is: <?php echo $_POST["major"]; ?><br>
+Here are your comments: <?php echo $_POST["comment"]; ?><br>
+
+</body>
+</html>
