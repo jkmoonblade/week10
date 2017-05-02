6d6f193e4a79add1fe3794aa83a151b4c21aa4d6
Joseph Keene <Joseph Keene>
2017-05-02 14:28:10 -0600

    wip...

--- web/browse03.php
+++ web/browse03.php
@@ -7,17 +7,28 @@ session_start();
 <html>
 <body>
 
-<h1 align="center">Joe's Click and Ship!</h1>
+<div align="center">
+<h1>Joe's Click and Ship!</h1>
 
 <p>Tired of paying for stuff online?* Worry no more! With Joe's Click and Ship, you can find items you like, click on it, and have it ship to your house! It's that simple!**</p>
 
-<h2 align="center">Come on In!</h2>
-<p align="center">I knew you'd want to check out these sweet deals.</p>
+<h2>Come on In!</h2>
+<p>I knew you'd want to check out these sweet deals.</p>
+</div>
 
 <form method="post" action="browse03.php" method="post">
 
 	<p>What's your favorite color?</p>
 	<input type="text" name="color"><br>
+
+	<p>Check the boxes next to items you'd like to add to your cart.</p>
+	<input type="checkbox" name="items[]" value="Tissue box">Tissue box
+	<input type="checkbox" name="items[]" value="Toilet plunger">Toilet plunger
+	<input type="checkbox" name="items[]" value="Ramen">Ramen
+	<input type="checkbox" name="items[]" value="Forks">Forks
+	<input type="checkbox" name="items[]" value="Couch">Couch
+	<input type="checkbox" name="items[]" value="Free Healthcare">Free Healthcare
+	<input type="checkbox" name="items[]" value="Hope">Hope<br>
 
 <input type="submit">
 </form>
@@ -25,7 +36,14 @@ session_start();
 
 <?php 
 $_SESSION["favcolor"] = $_POST["color"]; 
-echo "Favorite color is " . $_SESSION["favcolor"];
+$_SESSION["items"] = $_POST["items"];
+
+echo "Favorite color is: " . $_SESSION["favcolor"];
+foreach ($items as $item)
+{
+	$item_clean = htmlspecialchars($item);
+	echo "<li><p>$item_clean</p></li>";
+}
 ?>
 
 <p>*  You will be required to pay for items when they arrive on your doorstep.</p>
