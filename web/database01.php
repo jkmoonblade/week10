10abcf4a77180e187196f6c642383dd2501d292d
Joseph Keene <Joseph Keene>
2017-05-20 10:29:11 -0600

    ...

--- web/database01.php
+++ web/database01.php
@@ -12,7 +12,8 @@ try
 	$dbPort = $dbopts["port"];
 	$dbUser = $dbopts["user"];
 	$dbPassword = $dbopts["pass"];
-	$dbName = ltrim($dbopts["path"],'/');
+	$dbName = 'project1';
+	//$dbName = ltrim($dbopts["path"],'/');
 
 	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 }
