a77e3084315e492e8624ff58a56450afcd21f16a
Joseph Keene <Joseph Keene>
2017-05-20 10:43:43 -0600

    ...

--- web/database01.php
+++ web/database01.php
@@ -1,19 +1,43 @@
+<html>
+<body>
+
 <?php
-// Start the session
-session_start();
 
-try
-{
-  	$user = 'postgres';
-  	$password = 's22554';
- 	$db = new PDO('pgsql:host=localhost;dbname=project1', $user, $password);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
+// default Heroku Postgres configuration URL
+$dbUrl = getenv('DATABASE_URL');
+
+if (empty($dbUrl)) {
+ // example localhost configuration URL with postgres username and a database called cs313db
+ $dbUrl = "postgres://postgres:s22554@localhost:5432/project1";
 }
 
-echo "Connected to " . $dbName;
+$dbopts = parse_url($dbUrl);
 
+print "<p>$dbUrl</p>\n\n";
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+
+try {
+ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+catch (PDOException $ex) {
+ print "<p>error: $ex->getMessage() </p>\n\n";
+ die();
+}
+
+foreach ($db->query('SELECT now()') as $row)
+{
+ print "<p>$row[0]</p>\n\n";
+}
+
+?>
+
+</body>
+</html>
 ?>
