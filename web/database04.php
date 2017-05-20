418fad47a73f18b1e02100a005e6b7fc0d5d2715
Joseph Keene <Joseph Keene>
2017-05-20 15:11:06 -0600

    working on the database still

---
+++ web/database04.php
@@ -0,0 +1,41 @@
+<html>
+<body>
+
+<?php
+
+// default Heroku Postgres configuration URL
+$dbUrl = getenv('DATABASE_URL');
+
+if (empty($dbUrl)) {
+ // example localhost configuration URL with postgres username and a database called cs313db
+ $dbUrl = "postgres://postgres:s22554@localhost:5432/project1";
+}
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+try {
+ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+catch (PDOException $ex) {
+ print "<p>error: $ex->getMessage() </p>\n\n";
+ die();
+}
+
+$statement = $db->query('SELECT pokemon_id, attack1, attack2 FROM public.attacks');
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+  echo 'Pokemon ID: ' . $row['pokemon_id'] . ' attack 1: ' . $row['attack1'] . ' attack 2: ' . $row['attack2'] . '<br/>';
+}
+
+?>
+
+<a href="database01.php">Return to Home</a>
+
+</body>
+</html>
